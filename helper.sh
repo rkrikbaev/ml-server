3a3a4f3373472ca27355b92c53664f35c2d3d023
rkrikbaev <rkrikbaev@gmail.com>
2024-10-26 20:25:18 +0500

    add test comand to helper

--- helper.sh
+++ helper.sh
@@ -1,4 +1,5 @@
-docker run -it --name ml0 --restart always -p 8000 -m 1024m -v .\Downloads\kegoc_services\ml\server:/server fpcloud/ml:0.0.1 python3 -m uvicorn server:app --host 0.0.0.0 --port 8000
+docker run -it --name ml0 --restart always -p 8000 -m 1024m \
+-v .\Downloads\kegoc_services\ml\server:/server fpcloud/ml:0.0.1 python3 -m uvicorn server:app --host 0.0.0.0 --port 8000
 
 # start with bash
 docker run -itd --rm --name model1 -p 18200:8000 \
