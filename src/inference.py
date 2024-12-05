Fix units & grouping in get_full_days_mask

src/inference.py
@@ -20,16 +20,17 @@ from utils import timestamps_to_calendar_features, load_model_and_normalization
 
 def get_full_days_mask(timestamps: List[np.array], max_n_days=None):
     df = pd.DataFrame({'dt': timestamps})
-    df['dt'] = pd.to_datetime(df['dt'], unit='ns')
+    df['dt'] = pd.to_datetime(df['dt'], unit='ms')
     
-    df['date_counts'] = df.groupby(df['dt'].dt.date).transform('count')
+    df['date_counts'] = df.groupby(df['dt'].dt.round('D')).transform('count')
     mask = df['date_counts'] == df['date_counts'].max()
 
     if max_n_days is None:
         return mask
 
     cutoff_date = df[mask]['dt'].iloc[-1] - pd.Timedelta(days=max_n_days)
-    return mask & (df['dt'] > cutoff_date)
+    mask = mask & (df['dt'] > cutoff_date)
+    return mask.values
 
 
 def extract_features(