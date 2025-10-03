8772fe20c84ebf90d796ff68276d676fb962514b
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2025-10-03 16:02:59 +0500

    Add init script

---
+++ fp/prototypes/model_control/init.js
@@ -0,0 +1,132 @@
+async function(VARS,element,context){
+    window.__dialog_screen = element;
+    
+    fp.rt.get_server_time((time) => {
+        console.log("@models");
+        const pattern = '/root/FP/prototypes/model_control/fields';
+        const path = window.__worker;
+        console.log(path);
+                
+                
+                
+        let colorList = [
+        "#c9252d",
+        "#12805c",
+        "#cb6f10",
+        "#6f38b1"
+    ];
+    // console.log(path);
+    if(path && path.length > 0){
+        
+        const split_path = path.split("/");
+        const model_name = split_path[split_path.length - 1];
+        const query = `GET model_input from * WHERE AND(.path = '${path}', .pattern = $oid('${pattern}') )`;
+        let arr = [];
+        
+        
+        context.get_connection().find(query, async (result) => {
+            console.log("result: ");
+            console.log(result);
+            for(let i = 0; i < result.total; i++){
+                if(result.set[i].fields.model_input){
+                   for(let j = 0; j < result.set[i].fields.model_input.length; j++) {
+                        if(j === 0) {
+                            arr.push(
+                            {
+                                  "oid": result.set[i].fields.model_input[j],
+                                  "caption": result.set[i].fields.model_input[j].replace("/root/FP/PROJECT", ""),
+                                  "type": "archive",
+                                  "axis": "y",
+                                  "group": "Fact",
+                                  "color": "#000000",
+                                  "settings": {
+                                    "strokeWidth": 1,
+                                    "pointSize": 1,
+                                    "drawPoints": false,
+                                    "stepPlot": true,
+                                    "visibility": "on"
+                                  }
+                                }
+                             );
+                        } else if (j === 1) {
+                            arr.push(
+                                {
+                                  "oid": result.set[i].fields.model_input[j],
+                                  "caption": result.set[i].fields.model_input[j].replace("/root/FP/PROJECT", ""),
+                                  "axis": "y2",
+                                  "type": "archive",
+                                  "group": "Fact",
+                                  "color": colorList[j],
+                                  "settings": {
+                                    "strokeWidth": 1,
+                                    "pointSize": 1,
+                                    "drawPoints": false,
+                                    "stepPlot": true,
+                                    "visibility": "on"
+                                  }
+                                }
+                             );
+                        } else {
+                            arr.push(
+                                {
+                                  "oid": result.set[i].fields.model_input[j],
+                                  "caption": result.set[i].fields.model_input[j].replace("/root/FP/PROJECT", ""),
+                                  "axis": "y",
+                                  "type": "archive",
+                                  "group": "Fact",
+                                  "color": colorList[j],
+                                  "settings": {
+                                    "strokeWidth": 1,
+                                    "pointSize": 1,
+                                    "drawPoints": false,
+                                    "stepPlot": true,
+                                    "visibility": "on"
+                                  }
+                                }
+                             );
+                        }
+                    } 
+                }
+            }
+            arr.push(
+                {
+                  "oid": path + "/archives/out_value",
+                  "caption": "out_value",
+                  "axis": "y",
+                  "type": "archive",
+                  "group": "Predict",
+                  "color": "#0d66d0",
+                  "settings": {
+                    "strokeWidth": 1,
+                    "pointSize": 1,
+                    "drawPoints": false,
+                    "stepPlot": true,
+                    "visibility": "on"
+                  }
+                }
+             );
+            console.log("arr", arr);
+            context.__trend_element.pause();
+            const oneDay = 24*60*60*1000;
+            const from = Math.floor((time - 7 * oneDay) / oneDay) * oneDay - 5 * 60 * 60 * 1000;
+            const to = from + 9 * oneDay;
+            // console.log(from, to);
+            
+            context.__trend_element.set({archives:arr});
+            
+            const values = await context.__trend_element.load_from_to({from, to});
+            
+            console.log(values);
+            context.__trend_element.update_dygraph(values);
+            
+            context.__trend_element.init_description();
+            
+            context.__trend_element.period.set({value : {from,to}});
+           
+            
+        },(e) => {
+            console.log(`Set archive error:`, e);
+        },5000);
+        }
+    })
+}
