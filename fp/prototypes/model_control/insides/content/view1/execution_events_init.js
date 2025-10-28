6433a0382a844054e7792e50357e8384ef43d408
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2025-10-28 16:09:49 +0500

    Add execution init script

---
+++ fp/prototypes/model_control/insides/content/view1/execution_events_init.js
@@ -0,0 +1,27 @@
+function(VARS,element,context){
+    
+    const path = window.__worker;
+    
+    if(path !== null && path !== undefined && path.length > 0){
+        const split_path = path.split("/");
+        const model_object = split_path[split_path.length - 1];
+        const pattern = '/root/FP/prototypes/model_control/fields';
+        
+        
+        const query = `GET task_status, task_message, task_updated, title from * WHERE AND(.path = '${path}', .pattern = $oid('${pattern}') )`;
+        
+        context.get_connection().find(query, (result) => {
+                if(result.set.length >= 1){
+                    context.__status.set({value : result.set[0].fields.task_status});
+                    context.__message.set({value : result.set[0].fields.task_message});
+                    context.__updated.set({value : result.set[0].fields.task_updated});
+                    context.__model_name.set({value : result.set[0].fields.title});
+                }
+                
+            },(e) => {
+                console.log(`Set settings data error:`, e);
+        },5000);
+    }
+    
+    
+}
