e8b224c9ed3bb2613a5ef0f4434b83068f013db2
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2025-10-29 09:56:14 +0500

    Cleanup

--- fp/prototypes/model_control/insides/content/view1/execution_events_init.js
+++ fp/prototypes/model_control/insides/content/view1/execution_events_init.js
@@ -3,11 +3,7 @@ function(VARS,element,context){
     const path = window.__worker;
     
     if(path !== null && path !== undefined && path.length > 0){
-        const split_path = path.split("/");
-        const model_object = split_path[split_path.length - 1];
         const pattern = '/root/FP/prototypes/model_control/fields';
-        
-        
         const query = `GET task_status, task_message, task_updated, title from * WHERE AND(.path = '${path}', .pattern = $oid('${pattern}') )`;
         
         context.get_connection().find(query, (result) => {
