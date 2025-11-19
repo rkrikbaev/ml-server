d0c3b5e4098d1877a3721a05a1ece5d82b519c1b
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2025-11-19 19:11:28 +0500

    Add rounding for hours and days

--- src/inference.py
+++ src/inference.py
@@ -303,15 +303,23 @@ def predict(
             model.fit(df_train)
 
         # The data interval middle is actually the current time
-        last_past_index = get_last_past_index(timestamps)
+        last_past_index = get_last_past_index(timestamps[0])
         pred_start_dt = pd.to_datetime(timestamps[0][last_past_index+1], unit='ms')
         if step == 2592000000:
             # Round to current month start
             pred_start_dt = pred_start_dt.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
             pred_dt = [pred_start_dt + pd.DateOffset(months=i) for i in range(output_range)]
+        elif step == 86400000:
+            # Round to current day start
+            pred_start_dt = pred_start_dt.replace(hour=0, minute=0, second=0, microsecond=0)
+            pred_dt = [pred_start_dt + pd.DateOffset(days=i) for i in range(output_range)]
+        elif step == 3600000:
+            # Round to current hour start
+            pred_start_dt = pred_start_dt.replace(minute=0, second=0, microsecond=0)
+            pred_dt = [pred_start_dt + pd.DateOffset(hours=i) for i in range(output_range)]
         else:
-            # Do not round for daily and hourly steps
-            pred_dt = [pred_start_dt + pd.DateOffset(milliseconds=step * i) for i in range(output_range)]
+            # Do not round for other steps
+            pass            
 
         df_future = pd.DataFrame({'ds': pred_dt})
         df_forecast = model.predict(df_future)
