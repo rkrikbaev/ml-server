9b67cc2356716c5d0a9c52aff1b14187a028a153
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2025-11-19 19:09:01 +0500

    Fix units

--- src/server.py
+++ src/server.py
@@ -51,7 +51,7 @@ async def _process_data(request: Request):
 
     try:
         step = d["step"]  # in seconds
-        period = (d["period"] * 3600) // d["step"]  # convert period from hours to number of timestamps
+        period = (d["period"] * 3600000) // d["step"]  # convert period from hours to number of timestamps
         task_id = d["task_id"]
         model_path = d.get("model_path", None)
         online = model_path == 'none'
