d289f8b7eb493a593b251b9c0e1df5ce74016b1a
Mikhail Kotyushev <mikhail.kotyushev@faceplate.io>
2026-02-24 16:01:24 +0500

    Add debug messages

--- src/server.py
+++ src/server.py
@@ -171,6 +171,7 @@ async def _process_data(request: Request):
     except Exception as e:
         logger.error(e)
         response["state"]["message"] = f"{STATUS_DATA_FORMAT_ERROR}: Invalid input JSON"
+        logger.debug(f'{response=}')
         return response
 
     # -------- extract input data
@@ -184,6 +185,7 @@ async def _process_data(request: Request):
     except Exception as e:
         logger.error(e)
         response["state"]["message"] = f"{STATUS_DATA_FORMAT_ERROR}: Invalid input dataset format"
+        logger.debug(f'{response=}')
         return response
 
     # -------- input QDS evaluation
@@ -225,6 +227,7 @@ async def _process_data(request: Request):
     except Exception as e:
         logger.error(e)
         response["state"]["message"] = f"{STATUS_EXECUTION_ERROR}: Forecast execution error"
+        logger.debug(f'{response=}')
         return response
 
     # -------- distribution mismatch
@@ -259,6 +262,7 @@ async def _process_data(request: Request):
     else:
         msg = f"{status}: {reason}" if reason else status
     response["state"] = {"quality": final_qds, "message": msg, "model_confidence": 1.0}
+    logger.debug(f'{response=}')
 
     return response
 
