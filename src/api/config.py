b36a5926b135d8bb1a05ddc7b9c272602fab1f90
Mariya Polkovnikova <mariya.polkovnikova@faceplate.io>
2026-03-16 17:17:58 +0500

    chore: redis timeout for broker

--- src/api/config.py
+++ src/api/config.py
@@ -5,7 +5,7 @@
 
 REDIS_URL = "redis://redis:6379/0"
 # REDIS_URL = "redis://localhost:6379/0"
-REDIS_TIMEOUT = 600  # 10 minutes
+REDIS_TIMEOUT = 3600  # 1 hour
 
 NDC_HOSTS = ["192.168.50.141"]
 # NDC_HOSTS = ["10.210.1.11", "10.210.1.13", "10.210.1.15"]
