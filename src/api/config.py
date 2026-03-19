cba785fa87e3ea7ed153540aab08d579ee364e55
Mariya Polkovnikova <mariya.polkovnikova@faceplate.io>
2026-03-19 11:38:39 +0500

    chore: update constants

--- src/api/config.py
+++ src/api/config.py
@@ -11,14 +11,12 @@ from os import getenv
 # -- Redis ---
 
 REDIS_URL = "redis://redis:6379/0"
-# REDIS_URL = "redis://localhost:6379/0"
 REDIS_TIMEOUT = 3600  # 1 hour
 
 
 # -- NDC ---
 
-NDC_HOSTS = ["192.168.50.141"]
-# NDC_HOSTS = ["10.210.1.11", "10.210.1.13", "10.210.1.15"]
+NDC_HOSTS = ["10.210.1.11", "10.210.1.13", "10.210.1.15"]
 NDC_URLS = [f"http://{host}:7080/api/read/archive" for host in NDC_HOSTS]
 
 
